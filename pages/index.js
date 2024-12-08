ac6867aac8cdc5dce5cc6cece30dba612a3ad8c9
Troysagents <troyt@tmtindustrial.com.au>
2024-12-08 19:26:15 +1000

    Fix blank screen issue - update main page

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,22 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ height: '100vh', margin: 0, padding: 0 }}>
       <Head>
-        <title>Search Interface</title>
+        <title>Web Search Interface</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div style={{ padding: '20px' }}>
-        <iframe 
-          src="/search.html" 
-          style={{
-            width: '100%',
-            height: '100vh',
-            border: 'none',
-            margin: 0,
-            padding: 0,
-          }}
-        />
-      </div>
+      <iframe 
+        src="/search.html" 
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          position: 'fixed',
+          top: 0,
+          left: 0
+        }}
+      />
     </div>
   );
 }
