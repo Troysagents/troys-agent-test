cbe31d7353f3c260433b040c32694f03bdf9f51f
Troysagents <troyt@tmtindustrial.com.au>
2024-12-08 19:24:21 +1000

    Fix main page component

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,23 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div style={{ height: '100vh', margin: 0, padding: 0 }}>
+    <div>
       <Head>
-        <title>Web Search Interface</title>
+        <title>Search Interface</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main style={{ height: '100%', width: '100%' }}>
+      <div style={{ padding: '20px' }}>
         <iframe 
           src="/search.html" 
           style={{
             width: '100%',
-            height: '100%',
+            height: '100vh',
             border: 'none',
             margin: 0,
-            padding: 0
+            padding: 0,
           }}
         />
-      </main>
+      </div>
     </div>
   );
 }
